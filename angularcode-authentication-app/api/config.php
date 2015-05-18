a67f1579ae5a75bf291d25c5f4855fe29e41309f
Wallingford Hockey Club <dev@wallingfordhc.org.uk>
2015-05-18 17:42:38 +0100

    Update config.php
    Changed DB login credentials

--- angularcode-authentication-app/api/config.php
+++ angularcode-authentication-app/api/config.php
@@ -2,9 +2,9 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'walling1_web');
+define('DB_PASSWORD', 'Hockey123');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'angularcode');
+define('DB_NAME', 'walling1_wildcats');
 
 ?>
